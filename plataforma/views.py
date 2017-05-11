fbd991ee9efc57223ff2aa1ffb1fdc267c31fb52
Paulo Bridi <paulobridi@gmail.com>
2017-05-11 17:33:53 -0300

    Views e URLs para novos templates

--- plataforma/views.py
+++ plataforma/views.py
@@ -7,3 +7,28 @@ from django.views.generic import TemplateView
 class LandingPageView(TemplateView):
 
     template_name = "pages/landing_page.html"
+
+
+class LoginView(TemplateView):
+
+    template_name = "pages/login.html"
+
+
+class CreateAccountView(TemplateView):
+
+    template_name = "pages/create_account.html"
+
+
+class NosView(TemplateView):
+
+    template_name = "pages/nos.html"
+
+
+class NosDetailView(TemplateView):
+
+    template_name = "pages/nos_detail.html"
+
+
+class ObjectDetailView(TemplateView):
+
+    template_name = "pages/object_detail.html"
