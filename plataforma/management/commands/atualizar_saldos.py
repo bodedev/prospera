e9a81e98b794bd54b56fd6a12f1d522f0586b2fd
Cícero Raupp Rolim <ironworld@gmail.com>
2017-07-13 11:20:42 -0300

    Agora atualizando o saldo bonitinho. :)

--- plataforma/management/commands/atualizar_saldos.py
+++ plataforma/management/commands/atualizar_saldos.py
@@ -8,24 +8,38 @@ from plataforma.models import Saldo
 import requests
 
 
+def buscar_saldo(carteira):
+    try:
+        r = requests.get("https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=%s&address=%s&tag=latest&apikey=%s" % (settings.ETHERSCAN_CONTRACT_ADDRESS, carteira, settings.ETHERSCAN_APIKEY))
+        if r.status_code == 200:
+            data = r.json()
+            if data["status"] == "1":
+                saldo = float(data["result"]) / float(ETHER_DIVISOR)
+                _, created = Saldo.objects.update_or_create(carteira=carteira, defaults={"total": saldo})
+                print "%s: %0.6f (%s)" % (carteira, saldo, str(created))
+                return True
+        return False
+    except Exception, e:
+        print "Nao consegui pegar o saldo da carteira %s" % carteira
+        return None
+
+
 class Command(BaseCommand):
 
     help = u"Atualiza o saldo de todas as carteiras de um contrato."
 
     def handle(self, *args, **options):
-        url = "https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=%s&toBlock=latest&address=%s&topic1=0x000000000000000000000000455f6c073f3898e53d9003b1d665d39d4b0d8cf8&apikey=%s" % (settings.ETHERSCAN_START_BLOCK_NUMBER, settings.ETHERSCAN_CONTRACT_ADDRESS, settings.ETHERSCAN_APIKEY)
+        url = "https://api.etherscan.io/api?module=logs&action=getLogs&fromBlock=%s&toBlock=latest&address=%s&apikey=%s" % (settings.ETHERSCAN_START_BLOCK_NUMBER, settings.ETHERSCAN_CONTRACT_ADDRESS, settings.ETHERSCAN_APIKEY)
         r = requests.get(url)
         data = r.json()
+        saldos_atualizados = []
         for transacion in data["result"]:
-            carteira = transacion["topics"][2].replace("0x000000000000000000000000", "0x")
-            try:
-                r = requests.get("https://api.etherscan.io/api?module=account&action=tokenbalance&contractaddress=%s&address=%s&tag=latest&apikey=%s" % (settings.ETHERSCAN_CONTRACT_ADDRESS, carteira, settings.ETHERSCAN_APIKEY))
-                if r.status_code == 200:
-                    data = r.json()
-                    if data["status"] == "1":
-                        saldo = float(data["result"]) / float(ETHER_DIVISOR)
-                        _, created = Saldo.objects.update_or_create(carteira=carteira, defaults={"total": saldo})
-                        print "%s: %0.6f (%s)" % (carteira, saldo, str(created))
-            except Exception, e:
-                print "Nao consegui pegar o saldo da carteira %s" % carteira
+            carteira_from = transacion["topics"][1].replace("0x000000000000000000000000", "0x")
+            if carteira_from not in saldos_atualizados:
+                if buscar_saldo(carteira_from):
+                    saldos_atualizados.append(carteira_from)
+            carteira_to = transacion["topics"][2].replace("0x000000000000000000000000", "0x")
+            if carteira_to not in saldos_atualizados:
+                if buscar_saldo(carteira_to):
+                    saldos_atualizados.append(carteira_to)
         print "Fim de processo!"
